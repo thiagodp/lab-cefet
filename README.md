339778e795fdb0ad18c227bd292fc241a1c44a96
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-01-24 22:43:59 -0300

    Adiciona versão inicial

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # lab-cefet
+
+> Bash script para CMD (Windows) que visa instalar programas necessários ao Laboratórios do Bacharelado em Sistemas de Informação do CEFET/RJ Nova Friburgo
+
+## Como utilizar
+
+> Use por sua conta e risco ⚠️
+
+1. Baixe [install.bat](install.bat) e salve-o em `C:\` (ou outra pasta qualquer).
+2. Clique com o botão direito no botão Iniciar (logo do Windows), clique em "Terminal (Administrador)" e confirme para abrir o terminal.
+3. Execute `cd C:\` e então `install`.
+
+
+## Programas instalados
+
+> Todos os programas são instalados em `C:\dev` (ex. `C:\dev\git`).
+
+- Git
+- cURL
+- PHP
+- Composer
+- Apache
+- MariaDB - já inclui MySQL - instalados com usuário `root` e senha `root`
+- PHPMyAdmin
+- NodeJS
+- Bun
+- Putty
+- PNPM
+
+## Licença
+
+MIT ©️ [Thiago Delgado Pinto](https://github.com/thiagodp)
---
+++ uninstall.bat
@@ -0,0 +1,40 @@
+@echo off
+REM Script de desinstalação - Thiago Delgado Pinto
+
+net session >nul 2>&1 || (echo E' preciso executar como Administrador && exit 1)
+
+cd C:\dev || (mkdir C:\dev && cd dev) || exit
+
+REM ---------------------------------------------------------------------------
+
+echo Desinstalando - por favor, aguarde...
+winget uninstall --nowarn -e --id cURL.cURL
+winget uninstall --nowarn -e --id Git.Git
+winget uninstall --name XAMPP
+winget uninstall --nowarn -e --id PHP.PHP.8.1
+winget uninstall --nowarn -e --id PHP.PHP.8.2
+winget uninstall --nowarn -e --id PHP.PHP.8.3
+winget uninstall --nowarn -e --id PHP.PHP.8.4
+winget uninstall --nowarn -e --id PHP.PHP.8.5
+
+httpd -k stop
+httpd -k uninstall
+winget uninstall --nowarn -e --id ApacheLounge.httpd
+
+sc stop MySQL
+mysqld --uninstall
+winget uninstall --nowarn -e --id MariaDB.Server
+
+npm uninstall -g pnpm
+
+winget uninstall --nowarn -e --id OpenJS.NodeJS.LTS
+winget uninstall --nowarn -e --id Oven-sh.Bun
+winget uninstall --nowarn -e --id PuTTY.PuTTY
+
+
+rmdir /Q /S C:\dev\apache\htdocs\phpmyadmin
+rmdir /Q /S C:\dev\composer
+rmdir /Q /S C:\dev\mariadb
+
+cd C:\dev && dir
+echo Pronto.
