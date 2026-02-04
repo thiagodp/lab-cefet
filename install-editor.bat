2f0a8e86b8aedbf155e2f0e7f3d4209caecaf544
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-02-04 15:50:26 -0300

    Ajusta instalação das extensões do VSCode

--- install-editor.bat
+++ install-editor.bat
@@ -10,4 +10,12 @@ REM Coloca o vscode no PATH temporário, se não estiver, para instalar extensõ
 ( PATH | findstr C:\dev\vscode ) || set PATH=%PATH%;C:\dev\vscode
 
 echo Extensões do VSCode
-code --install-extension ritwickdey.liveserver && code --install-extension devsense.phptools-vscode && code --install-extension rangav.vscode-thunder-client && code --install-extension qwtel.sqlite-viewer && code --install-extension bmewburn.vscode-intelephense-client
+for %%E in (
+    ritwickdey.liveserver
+    devsense.phptools-vscode
+    rangav.vscode-thunder-client
+    qwtel.sqlite-viewer
+    bmewburn.vscode-intelephense-client
+) do (
+    call code --install-extension %%E
+)
