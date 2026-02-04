c190ed66925256989eddc6a3164188738f0186f7
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-02-04 16:01:17 -0300

    Ajusta install.bat

--- install.bat
+++ install.bat
@@ -4,10 +4,14 @@ REM Checa se é Administrador
 net session >nul 2>&1 || (echo Este script deve ser executado como Administrador && exit 1)
 cd C:\dev || (mkdir C:\dev && cd dev) || exit
 
-call install-editor.bat
-call install-cpp.bat
-call install-java.bat
-call install-python.bat
-call install-office.bat
-call install-ops.bat
-call install-web.bat
+for %%E in (
+    install-editor
+    install-cpp
+    install-java
+    install-python
+    install-office
+    install-ops
+    install-web
+) do (
+    call %%E
+)
