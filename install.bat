3b05058d20815ce0ef77e9a9dacd6fad4299f1e4
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-02-04 16:11:33 -0300

    Substitui a chamada com call no install.bat por conta de problema no lab

--- install.bat
+++ install.bat
@@ -4,14 +4,23 @@ REM Checa se é Administrador
 net session >nul 2>&1 || (echo Este script deve ser executado como Administrador && exit 1)
 cd C:\dev || (mkdir C:\dev && cd dev) || exit
 
-for %%E in (
-    install-editor
-    install-cpp
-    install-java
-    install-python
-    install-office
-    install-ops
-    install-web
-) do (
-    call %%E
-)
+REM for %%E in (
+REM     install-editor
+REM     install-cpp
+REM     install-java
+REM     install-python
+REM     install-office
+REM     install-ops
+REM     install-web
+REM ) do (
+REM     call %%E
+REM )
+
+
+install-editor.bat
+install-cpp.bat
+install-java.bat
+install-python.bat
+install-office.bat
+install-ops.bat
+install-web.bat
