5ec7dd5a33ebbf496f1c3b3b499804ec9e23a599
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-07-14 19:29:31 -0300

    Adiciona acesso explícito ao MinGW

--- install-cpp.bat
+++ install-cpp.bat
@@ -2,10 +2,16 @@
 @echo off
 echo -- CPP -------------------------------------------------------------------
 
+REM Checa se é Administrador
+net session >nul 2>&1 || (echo Este script deve ser executado como Administrador && exit 1)
+
 echo CodeBlocks com MinGW
 winget install -e --id CodeBlocks.CodeBlocks.MinGW -l C:\dev\codeblocks
 REM Ajusta acesso
 (wmic useraccount get name|findstr Aluno) && icacls "C:\dev\codeblocks" /grant Aluno:(OI)(CI)M /T >>saida.txt && icacls "C:\dev\codeblocks" /deny Aluno:(OI)(CI)(DE,DC) /T  >>saida.txt
+REM Assegura o acesso ao MinGW
+(wmic useraccount get name|findstr Aluno) && icacls "C:\dev\codeblocks\MinGW" /grant Aluno:(OI)(CI)M /T >>saida.txt
+
 
 echo Arduino IDE
 winget install -e --id ArduinoSA.IDE.stable -l C:\dev\arduino-ide
