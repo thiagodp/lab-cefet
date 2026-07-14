2cf9c71081aac204a945e7ee2432dbe12af569ae
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-07-14 19:56:49 -0300

    Retira ajuste de permissão redundante de install-cpp

--- install-cpp.bat
+++ install-cpp.bat
@@ -9,9 +9,6 @@ echo CodeBlocks com MinGW
 winget install -e --id CodeBlocks.CodeBlocks.MinGW -l C:\dev\codeblocks
 REM Ajusta acesso
 (wmic useraccount get name|findstr Aluno) && icacls "C:\dev\codeblocks" /grant Aluno:(OI)(CI)M /T >>saida.txt && icacls "C:\dev\codeblocks" /deny Aluno:(OI)(CI)(DE,DC) /T  >>saida.txt
-REM Assegura o acesso ao MinGW
-(wmic useraccount get name|findstr Aluno) && icacls "C:\dev\codeblocks\MinGW" /grant Aluno:(OI)(CI)M /T >>saida.txt
-
 
 echo Arduino IDE
 winget install -e --id ArduinoSA.IDE.stable -l C:\dev\arduino-ide
